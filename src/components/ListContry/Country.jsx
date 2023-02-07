d21b80304b79e8127128c35d30d910f8374321c7
Allassane <dickoallassane1997@gmail.com>
2023-02-07 09:03:37 +0100

    Add contry styling and basic structure

--- src/components/ListContry/Country.jsx
+++ src/components/ListContry/Country.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Country = () => {
   return (
-    <div className='country'>
-      Country 0
+    <div className='flex-center column country'>
+      <img src='https://flagcdn.com/us.svg' alt="Country Images" />
+      <div className="column detail">
+        <h4>United State of USA</h4>
+        <p>950</p>
+      </div>
     </div>
   )
 }
