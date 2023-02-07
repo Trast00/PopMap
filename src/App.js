4b669c776569f6c4893f7006af8381d89e05e1fc
Allassane <dickoallassane1997@gmail.com>
2023-02-07 07:44:29 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Detail from './pages/Detail';
-import { Routes ,Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/:topLevelDomain' element={<Detail />} />
-      </Routes>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/:topLevelDomain" element={<Detail />} />
+    </Routes>
+  </div>
+);
 
 export default App;
